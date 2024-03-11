e7f879164144f851f0e5fc95c7f15f0d11313916
Amots Hetzroni <amotsh@gmail.com>
2024-03-11 10:36:10 +0200

    Update quality.class.php

--- model/quality.class.php
+++ model/quality.class.php
@@ -14,6 +14,7 @@ class quality
         'noPics' => ['literal' => 'פריט ללא תמונה', 'callback' => 'renderNoPicsTable'],
         'reusedPictures' => ['literal' => 'תמונות בשימוש כפול', 'callback' => 'renderReusedPictures'],
         'orphandPictures' => ['literal' => 'תמונות שלא בשימוש', 'callback' => 'renderOrphanPictures'],
+        'nonMatchingFields' => ['literal' => 'שדות לא תואמים', 'callback' => 'renderMisMatchedFields'],
     ];
 
     public function __construct()
@@ -44,14 +45,9 @@ class quality
     }
     public function renderQualityPage()
     {
-        // Debug::dump($this->rt,'rt at ' . util::getCaller());
-        $indexAt = 2;
-        // if (isset($this->rt[$indexAt]) and !util::IsNullOrEmptyString($this->rt[$indexAt])) {
-        //     $funcName = $this->menu[$this->rt[$indexAt]]['callback'];
         if ($this->request) {
             $funcName = $this->menu[$this->request]['callback'];
             if (method_exists($this, $funcName)) {
-                // Debug::dump($funcName, 'callback name at ' . util::getCaller());
                 return $this->$funcName();
             }
         }
@@ -65,7 +61,10 @@ class quality
         foreach ($records as $key => $record) {
             $title = Collection::renderTitle($record);
             $list[] = <<<EOF
-                <tr><td>{$record['item_id']}</td><td>{$title}</td></tr>
+                <tr>
+                <td><a href="/inventory/editItem/{$record['item_id']}">ערוך</td>
+                <td>{$record['item_id']}</td><td>{$title}</td>
+                </tr>
                 EOF;
         }
         $pre = <<<EOF
@@ -100,36 +99,19 @@ class quality
     private function renderOrphanPictures()
     {
         $data = $this->getorphanPics();
-        // Debug::dump($data, 'orphan pictures at ', util::getCaller());
-        $list = [];    
-        foreach($data as $item) {
+        $list = [];
+        foreach ($data as $item) {
             $list[] = <<<EOF
             <tr><td class="ltr">{$item}</td></tr>
             EOF;
-        }$pre = <<<EOF
+        }
+        $pre = <<<EOF
             <table id="list2Sort" class="table table-sm table-hover table-responsive tablesorter">
                 <thead><tr><th>file name</th></tr></thead><tbody>
             EOF;
-    $post = "</tbody></table>";
-    return $pre . join('', $list) . $post;
-    }
-    /* private function renderItemsList($records)
-    {
-        $list = [];
-        foreach ($records as $key => $record) {
-            $title = tractorCollection::renderTitle($record);
-            $list[] = <<<EOF
-                <tr><td>{$record['item_id']}</td><td>{$title}</td></tr>
-                EOF;
-        }
-        $pre = <<<EOF
-            <table id="list2sort" 
-                class="table table-sm table-hover table-responsive tablesorter tablesorter-default">
-                <thead><tr><th>מספר</th><th>כותר</th></tr></thead><tbody>
-            EOF;
         $post = "</tbody></table>";
         return $pre . join('', $list) . $post;
-    } */
+    }
 
     private function get_noPics()
     {
@@ -184,19 +166,9 @@ class quality
     {
         $directory = __SITE_PATH . '/assets/media/pics/items';
         $scanned_directory = array_diff(scandir($directory, SCANDIR_SORT_ASCENDING), ['..', '.']);
-        // Debug::dump($scanned_directory, 'directory listing pictures at ', util::getCaller());
-        // Debug::dump(sizeof($scanned_directory), 'directory listing pictures at ', util::getCaller());
         $picsInUse = $this->getPicsInUse();
-        // Debug::dump($picsInUse, 'pics in use at ', util::getCaller());
-        // Debug::dump(sizeof($picsInUse), 'pics in use at ', util::getCaller());
-        // $diff = array_udiff($scanned_directory, $picsInUse, [$this, 'name_compare']);
         $diff = array_diff($scanned_directory, $picsInUse);
-        // $diff = array_diff($picsInUse,$scanned_directory);
-        // $diff = $this->name_compare($scanned_directory, $picsInUse);
-        // $diff= array_diff($diff,$picsInUse);
         $manualDiff = $this->findPath($diff);
-        // Debug::dump($manualDiff, 'manuall diff in use at ', util::getCaller());
-        // return array_diff($diff,$manualDiff);
         return $manualDiff;
     }
     private function getPicsInUse()
@@ -227,16 +199,6 @@ class quality
             }
         }
         return $retArray;
-
-        /* if (trim($a) == trim($b)) {
-            return 0;
-        } 
-        if (trim($a)<trim($b)){
-            return -1;
-        }
-        if (trim($a)>trim($b)) {
-            return 1;
-        } */
     }
     private function findPath($a)
     {
@@ -245,14 +207,71 @@ class quality
         $sqlStr = "SELECT * FROM `pictures` WHERE `path` = :path LIMIT 1";
         $stmt = $pdo->prepare($sqlStr);
         foreach ($a as $value) {
-            $stmt->bindValue(':path',$value);
+            $stmt->bindValue(':path', $value);
             $stmt->execute();
             $result = $stmt->fetch();
             if (!$result) {
                 $diff[] = $value;
             }
         }
-        // Debug::dump($diff, 'pics in use at ', util::getCaller());
         return $diff;
+    }
+    private function renderMisMatchedFields()
+    {
+        $fields2compare = [
+            ['title' => 'כותר', 'field1' => 'caption_he', 'field2' => 'caption_en'], 
+            ['title' => 'יצרן', 'field1' => 'companyHe', 'field2' => 'companyEn'],
+            ['title' => 'דגם', 'field1' => 'modelHe', 'field2' => 'modelEn'],
+        ];
+        $list = [];
+        foreach ($fields2compare as $r) {
+            $list = array_merge($list, $this->getMisMatchedFields($r));
+        }
+
+        return $this->renderList($list);
+    }
+    private function getMisMatchedFields($r)
+    {
+        $pdo = db::getInstance();
+        $sqlStr = <<<EOF
+            SELECT
+                *
+            FROM
+                `items`
+            WHERE
+                (
+                    `{$r['field1']}` IS NULL OR `{$r['field1']}` = ''
+                ) ^(
+                    `{$r['field2']}` is NULL OR `{$r['field2']}` = ''
+                ) and (`display` = 1)
+            EOF;
+        $stmt = $pdo->prepare($sqlStr);
+        try {
+            $stmt->execute();
+        } catch (Exception $ex) {
+            $_SESSION['errors'][] = $ex->getTraceAsString();
+            $_SESSION['errors'][] = $stmt->errorInfo();
+        }
+        $records = $stmt->fetchAll();
+        return $records;
+    }
+    private function renderList($records)
+    {
+        $list = [];
+        foreach ($records as $key => $record) {
+            $title = Collection::renderTitle($record);
+            $list[] = <<<EOF
+                <tr>
+                <td><a href="/inventory/editItem/{$record['item_id']}">ערוך</td>
+                <td>{$record['item_id']}</td><td>{$title}</td>
+                </tr>
+                EOF;
+        }
+        $pre = <<<EOF
+            <table id="list2Sort" class="table table-sm table-hover table-responsive tablesorter">
+                <thead><tr><th>מספר</th><th>כותר</th></tr></thead><tbody>
+            EOF;
+        $post = "</tbody></table>";
+        return $pre . join('', $list) . $post;
     }
 }
